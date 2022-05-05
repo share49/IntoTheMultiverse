761e6989f312f5228d11a8489cab722dc34a8b65
Ruxel <28849659+share49@users.noreply.github.com>
2022-05-05 16:49:28 -0700

    + Project setup

---
+++ IntoTheMultiverse/Views/CharactersViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CharactersViewController.swift
+//  IntoTheMultiverse
+//
+//  Created by Roger Pintó Diaz on 5/5/22.
+//
+
+import UIKit
+
+final class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
